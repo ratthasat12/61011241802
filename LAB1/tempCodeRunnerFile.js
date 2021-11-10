d7eefcf448923ffa989f49ed410bef3692518c69
ratthasat12 <ratthasatop@gmail.com>
2021-11-10 11:39:28 +0700

    first commit

---
+++ LAB1/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const cars = ["Toyota","Kubota","Mazda"];
+const planes = [];
+planes[4] = "F16";
+planes[1] = "Boing 747";
+console.log(cars);
+console.log(planes);
+const cities = new Array("Buriram",31000)
+console.log(cities[1]);
